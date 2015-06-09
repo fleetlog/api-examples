8553cac94fafde3afdb4deb5590e3e39c639507c
Visvk <viktor.sincak@gmail.com>
2015-06-09 12:36:51 +0200

    oauth2-resource-owner-password-credentials

---
+++ oauth2-resource-owner-password-credentials/app.js
@@ -0,0 +1,43 @@
+var request = require('request');
+
+var API_BASE_URL = 'https://api.fleetlog.com.au';
+var CLIENT_ID = process.env.FLEETLOG_CLIENT_ID || 'test';
+var CLIENT_SECRET = process.env.FLEETLOG_CLIENT_SECRET || 'clientsecret';
+
+var USERNAME = process.env.USERNAME || 'user@name.com';
+var PASSWORD = process.env.PASSWORD || 'secret';
+
+var auth_string = new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
+
+
+var payload = {};
+payload.grant_type = 'password';
+payload.password = PASSWORD;
+payload.username = USERNAME;
+
+
+request({
+  method: 'POST',
+  url: API_BASE_URL + '/v2/token',
+  headers: {
+    'Authorization': 'Basic ' + auth_string,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  form: payload
+}, function(error, response, body) {
+  var bodyObj, e;
+
+  if (!error && response.statusCode == 200) {
+    try {
+      bodyObj = JSON.parse(body);
+    } catch (_error) {
+      console.log(_error);
+      return;
+    }
+    console.log(bodyObj)
+
+  } else {
+    // show error
+    return console.log('Status code: ' + response.statusCode + ' Error:' + error);
+  }
+});
